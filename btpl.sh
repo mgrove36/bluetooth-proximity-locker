16be1890cf7d308ff2bbcbdf4be58f245bbc2ac8
Matthew Grove <matthew@grove.gq>
2019-04-04 15:12:28 +0100

    Add more commands to PATH

--- btpl.sh
+++ btpl.sh
@@ -1,2 +1,9 @@
 #/bin/sh
-python3 ~/.bluetooth-proximity-locking/btpl.py [bluetooth address] > /dev/null &
+if [ $# \< 2 ]
+then
+    python3 ~/.bluetooth-proximity-locking/btpl.py [bluetooth address] > /dev/null &
+elif [ "$1" = "-r" ] || [ "$1" = "--run" ]
+    python3 ~/.bluetooth-proximity-locking/btpl.py [bluetooth address] > /dev/null &
+elif [ "$1" = "-s" ] || [ "$1" = "--stop" ]
+    pkill ~/.bluetooth-proximity-locking/btpl.py
+fi
