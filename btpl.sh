f673ea0164dc568764b5bad623f7a8972c9d6a29
Matthew Grove <matthew@grove.gq>
2019-04-04 18:10:49 +0100

    Fix typos Elif statements wouldn't run, as there were no 'then' statements

--- btpl.sh
+++ btpl.sh
@@ -1,10 +1,11 @@
 #/bin/sh
-
 if [ $# \< 2 ]
 then
     python3 ~/.bluetooth-proximity-locking/btpl.py [bluetooth address] > /dev/null &
-elif [ "$1" = "-r" ] || [ "$1" = "--run" ]
+elif [ [ "$1" = "-r" ] || [ "$1" = "--run" ] ]
+then
     python3 ~/.bluetooth-proximity-locking/btpl.py [bluetooth address] > /dev/null &
-elif [ "$1" = "-s" ] || [ "$1" = "--stop" ]
+elif [ [ "$1" = "-s" ] || [ "$1" = "--stop" ] ]
+then
     pkill ~/.bluetooth-proximity-locking/btpl.py
 fi
